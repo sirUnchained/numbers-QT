438c8b532661617d8bf6544127e7080097369a37
sirunchained <bbehnam3831@gmail.com>
2026-01-23 11:55:12 +0330

    some updates ...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,8 +4,46 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , centralWidget(new QWidget)
+    , formLayout(new QFormLayout)
+    , input_number(new QLineEdit)
+    , label_input_number(new QLabel)
+    , label_input_number_base(new QLabel)
+    , label_target_number_base(new QLabel)
+    , label_result(new QLabel)
+    , calc_btn(new QPushButton)
+    , spin_target_base(new QSpinBox)
+    , spin_input_base(new QSpinBox)
+    , menuButton(new QPushButton)
 {
     ui->setupUi(this);
+
+    setWindowTitle("number application");
+    setCentralWidget(centralWidget);
+    centralWidget->setLayout(formLayout);
+
+    calc_btn->setText("calculate");
+    menuButton->setText("Menu");
+    menuButton->setFixedSize(40, 40);
+    menuButton->setStyleSheet("font-size: 20px;");
+
+    label_input_number->setText("enter your number :");
+    label_input_number_base->setText("input number base :");
+    label_target_number_base->setText("target number base :");
+    label_result->setText("result");
+    label_result->setAlignment(Qt::AlignCenter);
+
+    spin_input_base->setMaximum(16);
+    spin_input_base->setMinimum(2);
+    spin_target_base->setMaximum(16);
+    spin_target_base->setMinimum(2);
+
+    formLayout->addRow(label_input_number, input_number);
+    formLayout->addRow(label_input_number_base, spin_input_base);
+    formLayout->addRow(label_target_number_base, spin_target_base);
+    formLayout->addRow(calc_btn, label_result);
+
+    QObject::connect(calc_btn, &QPushButton::clicked, this, &MainWindow::calculate_resultt);
 }
 
 MainWindow::~MainWindow()
@@ -13,25 +51,47 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_calc_btn_clicked()
+void MainWindow::calculate_resultt()
 {
-    int input_base = 0, output_base = 0, result = 0;
-    QString user_number;
+    QString user_number = input_number->text();
+    int input_base = spin_input_base->value();
+    int output_base = spin_target_base->value();
 
-    user_number = ui->user_input->text();
-    input_base = ui->input_base->value();
-    output_base = ui->output_base->value();
-
-    ui->result->setText(calculate_result(user_number, input_base, output_base));
-}
-
-QString MainWindow::calculate_result(QString number, int input_base, int output_base)
-{
     bool ok = false;
-    int input_num = number.toInt(&ok, input_base);
-    if(!ok) return QString("Error, check input base is same as what you typed");
+    int input_num = user_number.toInt(&ok, input_base);
+    if (!ok) {
+        label_result->setText("Error, check input base is same as what you typed");
+        return;
+    }
 
     QString output_num = QString::number(input_num, output_base);
-    return output_num;
+    label_result->setText(output_num);
 }
 
+void MainWindow::setupMenu()
+{
+    menuWidget = new QWidget(this);
+    menuWidget->setFixedWidth(200);
+    menuWidget->setStyleSheet("background-color: #f0f0f0;");
+
+    menuLayout = new QVBoxLayout(menuWidget);
+
+    convertBasePageBtn = new QPushButton("🏠 Convert Numbers Base", this);
+
+    // Style menu buttons
+    QString buttonStyle = "QPushButton {"
+                          "text-align: left;"
+                          "padding: 15px;"
+                          "font-size: 14px;"
+                          "border: none;"
+                          "border-radius: 5px;"
+                          "}"
+                          "QPushButton:hover {"
+                          "background-color: #e0e0e0;"
+                          "}";
+
+    convertBasePageBtn->setStyleSheet(buttonStyle);
+
+    menuLayout->addWidget(convertBasePageBtn);
+    menuLayout->addStretch();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QFormLayout>
+#include <QLabel>
+#include <QLineEdit>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QSpinBox>
+#include <QStackedWidget>
+#include <QtDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,10 +25,32 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_calc_btn_clicked();
-
 private:
     Ui::MainWindow *ui;
+
+    QWidget *centralWidget;
+
+    // Main page widgets
+    QFormLayout *formLayout;
+    QLineEdit *input_number;
+    QLabel *label_input_number, *label_input_number_base, *label_target_number_base, *label_result;
+    QPushButton *calc_btn;
+    QSpinBox *spin_target_base, *spin_input_base;
+
+    // Menu widgets
+    QWidget *menuWidget;
+    QVBoxLayout *menuLayout;
+    QPushButton *menuButton;
+    QPushButton *convertBasePageBtn;
+
+    // Page container
+    QStackedWidget *stackedWidget;
+
+    // Pages
+    QWidget *convertBasePage;
+    QWidget *calculatorPage;
+
+    void calculate_resultt();
+    void setupMenu();
 };
 #endif // MAINWINDOW_H
